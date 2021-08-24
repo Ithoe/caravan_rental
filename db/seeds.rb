d9e924cd34e069f23b18f7225fc915e0367a5d12
Sander Boogaards <sanderboogaards@gmail.com>
2021-08-24 11:24:34 +0200

    Homepage with cards added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Removing all caravans..."
+Caravan.destroy_all
+
+puts "Adding caravans..."
+
+Caravan.create(name: "Super caravan", description: "Best caravan in the world and for a very good price", built_year: 1984, brand: "Airstream", daily_rate: 20, user_id: 1)
+Caravan.create(name: "Old caravan", description: "A real classic", built_year: 1962, brand: "Golden Oldie", daily_rate: 50, user_id: 1)
+Caravan.create(name: "Modern caravan", description: "A new caravan", built_year: 2020, brand: "Airstream", daily_rate: 80, user_id: 1)
+Caravan.create(name: "Big caravan", description: "For the entire family", built_year: 2015, brand: "Airstream", daily_rate: 120, user_id: 1)
+
+puts "Caravans added!"
